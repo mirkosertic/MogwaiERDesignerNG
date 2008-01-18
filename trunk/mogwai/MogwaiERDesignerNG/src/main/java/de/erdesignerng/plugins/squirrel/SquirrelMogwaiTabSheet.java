7765b41faa608d4d8d7b3b8f9efd7886f0175e06
mirkosertic <mirkosertic@d3b2640a-98bb-4239-95bf-c7bbe60608d9>
2008-01-18 21:05:36 +0000

    Added base structure for squirrel plugin

---
+++ trunk/mogwai/MogwaiERDesignerNG/src/main/java/de/erdesignerng/plugins/squirrel/SquirrelMogwaiTabSheet.java
@@ -0,0 +1,35 @@
+package de.erdesignerng.plugins.squirrel;
+
+import java.awt.Component;
+
+import net.sourceforge.squirrel_sql.client.session.ISession;
+import net.sourceforge.squirrel_sql.client.session.mainpanel.BaseMainPanelTab;
+
+public class SquirrelMogwaiTabSheet extends BaseMainPanelTab {
+
+    public SquirrelMogwaiTabSheet(ISession session, SquirrelMogwaiPlugin plugin) {
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    protected void refreshComponent() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public Component getComponent() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public String getHint() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public String getTitle() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
