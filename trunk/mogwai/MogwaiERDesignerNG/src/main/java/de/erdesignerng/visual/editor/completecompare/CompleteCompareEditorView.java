284f657f35a9e9fe664dfe7dfc1a3d4d2ba30b0f
mirkosertic <mirkosertic@d3b2640a-98bb-4239-95bf-c7bbe60608d9>
2008-02-09 13:28:57 +0000

    Added core complete compare functionality

---
+++ trunk/mogwai/MogwaiERDesignerNG/src/main/java/de/erdesignerng/visual/editor/completecompare/CompleteCompareEditorView.java
@@ -0,0 +1,147 @@
+/**
+ * Mogwai ERDesigner. Copyright (C) 2002 The Mogwai Project.
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+package de.erdesignerng.visual.editor.completecompare;
+
+import javax.swing.JPanel;
+
+import com.jgoodies.forms.factories.DefaultComponentFactory;
+import com.jgoodies.forms.layout.CellConstraints;
+import com.jgoodies.forms.layout.FormLayout;
+
+import de.mogwai.common.client.looks.components.DefaultButton;
+import de.mogwai.common.client.looks.components.DefaultTree;
+
+/**
+ * Visual class CompleteCompareEditorView.
+ * 
+ * Created with Mogwai FormMaker 0.7.
+ */
+public class CompleteCompareEditorView extends JPanel {
+
+    private JPanel contentPanel;
+
+    private DefaultTree currentModelView;
+
+    private DefaultTree databaseView;
+
+    private DefaultButton okButton;
+
+    /**
+     * Constructor.
+     */
+    public CompleteCompareEditorView() {
+        initialize();
+    }
+
+    /**
+     * Initialize method.
+     */
+    private void initialize() {
+
+        String rowDef = "8dlu,fill:250dlu,8dlu,p,2dlu";
+        String colDef = "2dlu,fill:60dlu,2dlu:grow,fill:60dlu,2dlu";
+
+        FormLayout layout = new FormLayout(colDef, rowDef);
+        setLayout(layout);
+
+        CellConstraints cons = new CellConstraints();
+
+        this.add(getContentPanel(), cons.xywh(2, 2, 3, 1));
+        this.add(getOkButton(), cons.xywh(2, 4, 1, 1));
+
+        buildGroups();
+    }
+
+    /**
+     * Getter method for component ContentPanel.
+     * 
+     * @return the initialized component
+     */
+    public JPanel getContentPanel() {
+
+        if (contentPanel == null) {
+            contentPanel = new JPanel();
+
+            String rowDef = "2dlu,p,2dlu,fill:200dlu:grow,2dlu";
+            String colDef = "fill:180dlu,2dlu:grow,fill:180dlu";
+
+            FormLayout layout = new FormLayout(colDef, rowDef);
+            contentPanel.setLayout(layout);
+
+            CellConstraints cons = new CellConstraints();
+
+            contentPanel.add(DefaultComponentFactory.getInstance().createSeparator("Current model"), cons.xywh(
+                    1, 2, 1, 1));
+            contentPanel.add(DefaultComponentFactory.getInstance().createSeparator("Database"), cons.xywh(3, 2,
+                    1, 1));
+            contentPanel.add(getCurrentModelView().getScrollPane(), cons.xywh(1, 4, 1, 1));
+            contentPanel.add(getDatabaseView().getScrollPane(), cons.xywh(3, 4, 1, 1));
+        }
+
+        return contentPanel;
+    }
+
+    /**
+     * Getter method for component CurrentModelView.
+     * 
+     * @return the initialized component
+     */
+    public DefaultTree getCurrentModelView() {
+
+        if (currentModelView == null) {
+            currentModelView = new DefaultTree();
+        }
+
+        return currentModelView;
+    }
+
+    /**
+     * Getter method for component DatabaseView.
+     * 
+     * @return the initialized component
+     */
+    public DefaultTree getDatabaseView() {
+
+        if (databaseView == null) {
+            databaseView = new DefaultTree();
+        }
+
+        return databaseView;
+    }
+
+    /**
+     * Getter method for component OkButton.
+     * 
+     * @return the initialized component
+     */
+    public DefaultButton getOkButton() {
+
+        if (okButton == null) {
+            okButton = new DefaultButton();
+        }
+
+        return okButton;
+    }
+
+    /**
+     * Initialize method.
+     */
+    private void buildGroups() {
+
+    }
+}
