653d2949f3fd938efada39ad48e6a2cc8b1fb2de
Mirko Sertic <mirko.sertic@web.de>
2008-11-11 17:48:43 +0000

    Implemented FR 1886304 [ERDesignerNG] Allow saving of model to database

--- src/main/java/de/erdesignerng/model/serializer/dictionary/DictionaryIndexSerializer.java
+++ src/main/java/de/erdesignerng/model/serializer/dictionary/DictionaryIndexSerializer.java
@@ -26,6 +26,8 @@ import org.hibernate.Session;
 
 import de.erdesignerng.model.Attribute;
 import de.erdesignerng.model.Index;
+import de.erdesignerng.model.IndexType;
+import de.erdesignerng.model.Model;
 import de.erdesignerng.model.Table;
 import de.erdesignerng.model.serializer.dictionary.entities.IndexEntity;
 import de.erdesignerng.model.serializer.dictionary.entities.TableEntity;
@@ -37,26 +39,55 @@ import de.erdesignerng.model.serializer.dictionary.entities.TableEntity;
  */
 public class DictionaryIndexSerializer extends DictionarySerializer {
 
+    private static final int TYPE_UNIQUE = 0;
+
+    private static final int TYPE_NONUNIQUE = 1;
+
+    private static final int TYPE_PK = 2;
+
+    private static final int TYPE_UNDEFINED = -1;
+
     public static final DictionaryIndexSerializer SERIALIZER = new DictionaryIndexSerializer();
 
     protected void copyExtendedAttributes(Index aSource, IndexEntity aDestination) {
         switch (aSource.getIndexType()) {
         case UNIQUE:
-            aDestination.setType(0);
+            aDestination.setType(TYPE_UNIQUE);
             break;
         case NONUNIQUE:
-            aDestination.setType(1);
+            aDestination.setType(TYPE_NONUNIQUE);
             break;
         case PRIMARYKEY:
-            aDestination.setType(2);
+            aDestination.setType(TYPE_PK);
             break;
         default:
-            aDestination.setType(-1);
+            aDestination.setType(TYPE_UNDEFINED);
         }
 
         aDestination.getAttributes().clear();
         for (Attribute theAttribute : aSource.getAttributes()) {
             aDestination.getAttributes().add(theAttribute.getSystemId());
+        }
+    }
+
+    protected void copyExtendedAttributes(IndexEntity aSource, Index aDestination, Table aTable) {
+        switch (aSource.getType()) {
+        case TYPE_UNIQUE:
+            aDestination.setIndexType(IndexType.UNIQUE);
+            break;
+        case TYPE_NONUNIQUE:
+            aDestination.setIndexType(IndexType.NONUNIQUE);
+            break;
+        case TYPE_PK:
+            aDestination.setIndexType(IndexType.PRIMARYKEY);
+            break;
+        default:
+            throw new RuntimeException("Invalid index type : " + aSource.getType());
+        }
+
+        aDestination.getAttributes().clear();
+        for (String theAttributeId : aSource.getAttributes()) {
+            aDestination.getAttributes().add(aTable.getAttributes().findBySystemId(theAttributeId));
         }
     }
 
@@ -95,5 +126,17 @@ public class DictionaryIndexSerializer extends DictionarySerializer {
             }
         }
 
+    }
+
+    public void deserialize(Model aModel, Table aTable, TableEntity aTableEntity, Session aSession) {
+        for (IndexEntity theIndexEntity : aTableEntity.getIndexes()) {
+            Index theIndex = new Index();
+
+            copyBaseAttributes(theIndexEntity, theIndex);
+            copyExtendedAttributes(theIndexEntity, theIndex, aTable);
+
+            theIndex.setOwner(aTable);
+            aTable.getIndexes().add(theIndex);
+        }
     }
 }
