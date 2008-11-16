0e63ec4941419b6ecf50e0d276c4f0e6eebd2590
mirkosertic <mirkosertic@d3b2640a-98bb-4239-95bf-c7bbe60608d9>
2008-11-16 11:18:39 +0000

    Implemented FR 1886304 [ERDesignerNG] Allow saving of model to database

--- trunk/mogwai/MogwaiERDesignerNG/src/main/java/de/erdesignerng/visual/editor/savetodictionary/SaveToDictionaryView.java
+++ trunk/mogwai/MogwaiERDesignerNG/src/main/java/de/erdesignerng/visual/editor/savetodictionary/SaveToDictionaryView.java
@@ -43,6 +43,8 @@ public class SaveToDictionaryView extends JPanel {
     private DefaultRadioButton existingEntryButton = new DefaultRadioButton(ERDesignerBundle.OVERWRITEEXISTINGENTRY);
 
     private DefaultComboBox existingNameBox = new DefaultComboBox();
+    
+    private DefaultTextField existingNameField = new DefaultTextField();
 
     private DefaultButton okButton = new DefaultButton();
 
@@ -55,7 +57,7 @@ public class SaveToDictionaryView extends JPanel {
     private void initialize() {
 
         String theColDef = "2dlu,10dlu,2dlu,50dlu,2dlu,fill:100dlu:grow,2dlu";
-        String theRowDef = "2dlu,p,2dlu,p,2dlu,p,2dlu,p,50dlu,2dlu,p,2dlu";
+        String theRowDef = "2dlu,p,2dlu,p,2dlu,p,2dlu,p,2dlu,p,50dlu,2dlu,p,2dlu";
 
         FormLayout theLayout = new FormLayout(theColDef, theRowDef);
         setLayout(theLayout);
@@ -70,6 +72,9 @@ public class SaveToDictionaryView extends JPanel {
         add(new DefaultLabel(ERDesignerBundle.NAME), cons.xy(4, 8));
         add(existingNameBox, cons.xy(6, 8));
 
+        add(new DefaultLabel(ERDesignerBundle.NEWNAME), cons.xy(4, 10));
+        add(existingNameField, cons.xy(6, 10));
+
         JPanel thePanel = new JPanel();
 
         theColDef = "60dlu,fill:2dlu:grow,60dlu";
@@ -83,7 +88,7 @@ public class SaveToDictionaryView extends JPanel {
         thePanel.add(cancelButton, cons.xy(3, 1));
         cancelButton.setText("Cancel");
 
-        add(thePanel, cons.xyw(2, 11, 5));
+        add(thePanel, cons.xyw(2, 13, 5));
     }
 
     /**
@@ -126,5 +131,12 @@ public class SaveToDictionaryView extends JPanel {
      */
     public DefaultButton getCancelButton() {
         return cancelButton;
+    }
+
+    /**
+     * @return the existingNameField
+     */
+    public DefaultTextField getExistingNameField() {
+        return existingNameField;
     }
 }
