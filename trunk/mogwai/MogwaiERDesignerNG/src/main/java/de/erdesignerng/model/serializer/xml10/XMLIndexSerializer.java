7131104911f6b42618f3bace1fcee14e70d19018
mirkosertic <mirkosertic@d3b2640a-98bb-4239-95bf-c7bbe60608d9>
2009-03-15 20:53:14 +0000

    Added better capsulation for different xml versions of the model

--- trunk/mogwai/MogwaiERDesignerNG/src/main/java/de/erdesignerng/model/serializer/xml10/XMLIndexSerializer.java
+++ trunk/mogwai/MogwaiERDesignerNG/src/main/java/de/erdesignerng/model/serializer/xml10/XMLIndexSerializer.java
@@ -21,7 +21,6 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 
-import de.erdesignerng.model.Attribute;
 import de.erdesignerng.model.Index;
 import de.erdesignerng.model.IndexExpression;
 import de.erdesignerng.model.IndexType;
