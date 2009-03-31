dfdf3e8f12f120f5c79574f3056ddb8d20670539
mirkosertic <mirkosertic@d3b2640a-98bb-4239-95bf-c7bbe60608d9>
2009-03-31 16:59:08 +0000

    Added first support of custom datatypes

--- trunk/mogwai/MogwaiERDesignerNG/src/main/java/de/erdesignerng/dialect/postgres/PostgresDialect.java
+++ trunk/mogwai/MogwaiERDesignerNG/src/main/java/de/erdesignerng/dialect/postgres/PostgresDialect.java
@@ -19,6 +19,7 @@ package de.erdesignerng.dialect.postgres;
 
 import java.sql.Types;
 
+import de.erdesignerng.dialect.DataType;
 import de.erdesignerng.dialect.NameCastType;
 import de.erdesignerng.dialect.sql92.SQL92Dialect;
 
@@ -35,43 +36,43 @@ public class PostgresDialect extends SQL92Dialect {
         setNullablePrimaryKeyAllowed(false);
         setCastType(NameCastType.NOTHING);
 
-        registerType(new PostgresDataType("bool", "", Types.BIT));
-        registerType(new PostgresDataType("bytea", "", Types.BINARY, Types.VARBINARY));
-        registerType(new PostgresDataType("name", "", Types.VARCHAR));
-        registerType(new PostgresDataType("int8", "", Types.BIGINT));
-        registerType(new PostgresDataType("bigserial", "", true, Types.BIGINT));
-        registerType(new PostgresDataType("int2", "", Types.SMALLINT, Types.TINYINT));
-        registerType(new PostgresDataType("int4", "", Types.INTEGER));
-        registerType(new PostgresDataType("serial", "", true, Types.INTEGER));
-        registerType(new PostgresDataType("text", "", Types.VARCHAR));
-        registerType(new PostgresDataType("oid", "", Types.INTEGER));
-        registerType(new PostgresDataType("float4", "", Types.REAL));
-        registerType(new PostgresDataType("float8", "", Types.DOUBLE, Types.FLOAT));
-        registerType(new PostgresDataType("money", "", Types.DOUBLE));
-        registerType(new PostgresDataType("bpchar", "", Types.CHAR));
-        registerType(new PostgresDataType("varchar", "$size", Types.VARCHAR));
-        registerType(new PostgresDataType("date", "", Types.DATE));
-        registerType(new PostgresDataType("time", "", Types.TIME));
-        registerType(new PostgresDataType("timestamp", "", Types.TIMESTAMP));
-        registerType(new PostgresDataType("timestamptz", "", Types.TIMESTAMP));
-        registerType(new PostgresDataType("timetz", "", Types.TIME));
-        registerType(new PostgresDataType("bit", "", Types.BIT));
-        registerType(new PostgresDataType("numeric", "$size,$fraction", Types.NUMERIC, Types.DECIMAL));
+        registerType(createDataType("bool", "", Types.BIT));
+        registerType(createDataType("bytea", "", Types.BINARY, Types.VARBINARY));
+        registerType(createDataType("name", "", Types.VARCHAR));
+        registerType(createDataType("int8", "", Types.BIGINT));
+        registerType(createDataType("bigserial", "", true, Types.BIGINT));
+        registerType(createDataType("int2", "", Types.SMALLINT, Types.TINYINT));
+        registerType(createDataType("int4", "", Types.INTEGER));
+        registerType(createDataType("serial", "", true, Types.INTEGER));
+        registerType(createDataType("text", "", Types.VARCHAR));
+        registerType(createDataType("oid", "", Types.INTEGER));
+        registerType(createDataType("float4", "", Types.REAL));
+        registerType(createDataType("float8", "", Types.DOUBLE, Types.FLOAT));
+        registerType(createDataType("money", "", Types.DOUBLE));
+        registerType(createDataType("bpchar", "", Types.CHAR));
+        registerType(createDataType("varchar", "$size", Types.VARCHAR));
+        registerType(createDataType("date", "", Types.DATE));
+        registerType(createDataType("time", "", Types.TIME));
+        registerType(createDataType("timestamp", "", Types.TIMESTAMP));
+        registerType(createDataType("timestamptz", "", Types.TIMESTAMP));
+        registerType(createDataType("timetz", "", Types.TIME));
+        registerType(createDataType("bit", "", Types.BIT));
+        registerType(createDataType("numeric", "$size,$fraction", Types.NUMERIC, Types.DECIMAL));
 
         // Patch [ 2124875 ] Add Postgres data types
-        registerType(new PostgresDataType("char", "$size", Types.CHAR));
-        registerType(new PostgresDataType("character", "$size", Types.CHAR));
-        registerType(new PostgresDataType("boolean", "", Types.BOOLEAN));
-        registerType(new PostgresDataType("interval", "", Types.TIMESTAMP));
-        registerType(new PostgresDataType("smallint", "", Types.SMALLINT));
-        registerType(new PostgresDataType("integer", "", Types.INTEGER));
+        registerType(createDataType("char", "$size", Types.CHAR));
+        registerType(createDataType("character", "$size", Types.CHAR));
+        registerType(createDataType("boolean", "", Types.BOOLEAN));
+        registerType(createDataType("interval", "", Types.TIMESTAMP));
+        registerType(createDataType("smallint", "", Types.SMALLINT));
+        registerType(createDataType("integer", "", Types.INTEGER));
 
-        registerType(new PostgresDataType("bigint", "", Types.BIGINT));
-        registerType(new PostgresDataType("real", "", Types.REAL));
-        registerType(new PostgresDataType("double precision", "", Types.DOUBLE));
-        registerType(new PostgresDataType("xml", "", Types.SQLXML));
-        registerType(new PostgresDataType("blob", "", Types.BLOB, Types.LONGVARBINARY));
-        registerType(new PostgresDataType("clob", "", Types.CLOB, Types.LONGVARCHAR));
+        registerType(createDataType("bigint", "", Types.BIGINT));
+        registerType(createDataType("real", "", Types.REAL));
+        registerType(createDataType("double precision", "", Types.DOUBLE));
+        registerType(createDataType("xml", "", Types.SQLXML));
+        registerType(createDataType("blob", "", Types.BLOB, Types.LONGVARBINARY));
+        registerType(createDataType("clob", "", Types.CLOB, Types.LONGVARCHAR));
 
         seal();
     }
@@ -120,4 +121,14 @@ public class PostgresDialect extends SQL92Dialect {
     public Class getHibernateDialectClass() {
         return org.hibernate.dialect.PostgreSQLDialect.class;
     }
+    
+    @Override
+    public DataType createDataType(String aName, String aDefinition, int... aJdbcType) {
+        return new PostgresDataType(aName, aDefinition, aJdbcType);
+    }
+
+    @Override
+    public DataType createDataType(String aName, String aDefinition, boolean aIdentity, int... aJdbcType) {
+        return new PostgresDataType(aName, aDefinition, aIdentity, aJdbcType);
+    }    
 }
