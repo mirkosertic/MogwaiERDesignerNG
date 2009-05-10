932f799f1b81adcb96cb605e1a1f494a08826b4e
Mirko Sertic <mirko.sertic@web.de>
2009-05-10 17:33:33 +0000

    Added H2 dialect contributed by Thomas Breitkreuz

---
+++ src/main/java/de/erdesignerng/dialect/h2/H2ReverseEngineeringStrategy.java
@@ -0,0 +1,60 @@
+/**
+ * Mogwai ERDesigner. Copyright (C) 2002 The Mogwai Project.
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+package de.erdesignerng.dialect.h2;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import de.erdesignerng.dialect.JDBCReverseEngineeringStrategy;
+import de.erdesignerng.dialect.SchemaEntry;
+
+/**
+ * @author $Author: gniddelgesicht $
+ * @version $Date: 2008/06/13 16:49:00 $
+ */
+public class H2ReverseEngineeringStrategy extends JDBCReverseEngineeringStrategy<H2Dialect> {
+
+    public H2ReverseEngineeringStrategy(H2Dialect aDialect) {
+        super(aDialect);
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<SchemaEntry> getSchemaEntries(Connection aConnection) throws SQLException {
+
+        List<SchemaEntry> theList = new ArrayList<SchemaEntry>();
+
+        DatabaseMetaData theMetadata = aConnection.getMetaData();
+        ResultSet theResult = theMetadata.getSchemas();
+
+        while (theResult.next()) {
+            String theSchemaName = theResult.getString("TABLE_SCHEM");
+            String theCatalogName = null;
+
+            theList.add(new SchemaEntry(theCatalogName, theSchemaName));
+        }
+
+        return theList;
+    }    
+}
