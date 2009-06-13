8f7eb117ad0792abde60f68cd8f5a24d0c4734f4
Mirko Sertic <mirko@mirkosertic.de>
2009-06-13 15:19:18 +0000

    Working on unit tests

--- src/test/resources/de/erdesignerng/test/sql/mssql/db.sql
+++ src/test/resources/de/erdesignerng/test/sql/mssql/db.sql
@@ -4,4 +4,4 @@ create view View1 as SELECT * from Table1
 create unique index Tabl11_idx1 on Table1 (tb2_2)
 alter table Table2 add constraint PK2 primary key (tb3_1)
 create index Tabl11_idx2 on Table1 (tb2_3)
-alter table Table1 add constraint FK1 foreign key (tb2_1) references Table2(tb3_1) on delete no action on update no action;
+alter table Table1 add constraint FK1 foreign key (tb2_1) references Table2(tb3_1) on delete no action on update no action
