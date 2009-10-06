7e12ec69f8db508a346394aef4ff44f103163881
Mirko Sertic <mirko.sertic@web.de>
2009-10-06 11:49:45 +0000

    Performance improvements

--- src/main/java/de/erdesignerng/model/Model.java
+++ src/main/java/de/erdesignerng/model/Model.java
@@ -249,19 +249,20 @@ public class Model implements OwnedModelItemVerifier {
         relations.remove(aRelation);
     }
 
-    public void removeAttributeFromTable(Table aTable, Attribute aAttribute) throws VetoException, ElementAlreadyExistsException, ElementInvalidNameException {
-        
+    public void removeAttributeFromTable(Table aTable, Attribute aAttribute) throws VetoException,
+            ElementAlreadyExistsException, ElementInvalidNameException {
+
         for (Index theIndex : aTable.getIndexes()) {
             if (theIndex.getExpressions().findByAttribute(aAttribute) != null) {
                 if (theIndex.getExpressions().size() == 1) {
                     // The index shall be dropped
                     removeIndex(aTable, theIndex);
                 } else {
-                    // The index must be modified                    
+                    // The index must be modified
                     removeIndex(aTable, theIndex);
-                    
+
                     theIndex.getExpressions().removeAttribute(aAttribute);
-                    
+
                     addIndexToTable(aTable, theIndex);
                 }
             }
