fe19dc5ed5ee2323c5d71820394b9b68ec5dff8d
Mirko Sertic <mirko@mirkosertic.de>
2009-11-06 17:09:03 +0000

    Enhanced Model30-DomainSerializer to manage domains with missing size attribute Added native MSAccess enums for RevEng relations and tables

---
+++ src/main/java/de/erdesignerng/dialect/msaccess/TableDefAttributeEnum.java
@@ -0,0 +1,94 @@
+/**
+ * Mogwai ERDesigner. Copyright (C) 2002 The Mogwai Project.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+package de.erdesignerng.dialect.msaccess;
+
+/**
+ * @author $Author: dr-death $
+ * @version $Date: 2009-11-06 14:15:00 $
+ * @see http://msdn.microsoft.com/en-us/library/bb225819.aspx
+ */
+public final class TableDefAttributeEnum {
+
+    public static final int DB_NONE = 0;                      // 0
+
+    /**
+     * Die Tabelle ist eine Systemtabelle, die vom Microsoft
+     * Jet-Datenbankmodul bereitgestellt wird. Sie können
+     * diese Konstante auf ein angefügtes TableDef-Objekt
+     * festlegen.
+     */
+    public static final int DB_SYSTEM_OBJECT = (-2^31 | 2^1); // -2147483646
+
+    /**
+     * Die Tabelle ist eine verborgene Tabelle, die vom
+     * Microsoft Jet-Datenbankmodul bereitgestellt wird. Sie
+     * können diese Konstante auf ein angefügtes TableDef-
+     * Objekt festlegen.
+     *
+     * Diese Einstellung kann *nicht* über das Access-GUI
+     * eingestellt werden.
+     */
+    public static final int DB_HIDDEN_OBJECT = 2^0;           // 1
+
+    /**
+     * Die Tabelle ist eine verborgene Tabelle, die vom
+     * Microsoft Jet-Datenbankmodul bereitgestellt wird. Sie
+     * können diese Konstante auf ein angefügtes TableDef-
+     * Objekt festlegen.
+     *
+     * Diese Einstellung kann über das GUI eingestellt
+     * werden - Eigenschaft "Ausgeblendet".
+     */
+    public static final int DB_INVISIBLE_OBJECT = 2^3;           // 8
+
+    /**
+     * Bei Datenbanken, die das Microsoft Jet-Datenbankmodul
+     * verwenden, ist die Tabelle eine verknüpfte Tabelle, die
+     * für den exklusiven Zugriff geöffnet wurde. Sie können
+     * die Konstanten auf einem angefügten TableDef-Objekt
+     * einer lokalen Tabelle festlegen, nicht jedoch für eine
+     * Remote-Tabelle.
+     */
+    public static final int DB_ATTACHED_EXCLUSIVE = 2^16;     // 65536
+
+    /**
+     * Bei Datenbanken, die das Microsoft Jet-Datenbankmodul
+     * verwenden, sind die Benutzerkennung und das Kennwort
+     * für die verknüpfte Tabelle zusammen mit den
+     * Verbindungsinformationen gespeichert. Sie können diese
+     * Konstanten auf ein hinzugefügtes TableDef-Objekt einer
+     * Remote-Tabelle festlegen, nicht jedoch auf eine lokale
+     * Tabelle.
+     */
+    public static final int DB_ATTACHED_SAVE_PWD = 2^17;      // 131072
+
+    /**
+     * Die Tabelle ist eine verknüpfte Tabelle aus einer ODBC-
+     * Datenbank, z.B. aus einer Datenbank des Microsoft SQL
+     * Server (schreibgeschützt).
+     */
+    public static final int DB_ATTACHED_ODBC = 2^19;          // 536870912
+
+    /**
+     * Die Tabelle ist eine verknüpfte Tabelle aus einer
+     * Nicht-ODBC-Datenquelle, z.B. aus einer Microsoft Jet-
+     * oder Paradox-Datenbank (schreibgeschützt).
+     */
+    public static final int DB_ATTACHED_TABLE = 2^30;         // 1073741824
+
+}
