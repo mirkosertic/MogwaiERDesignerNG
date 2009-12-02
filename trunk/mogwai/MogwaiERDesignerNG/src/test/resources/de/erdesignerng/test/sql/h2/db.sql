77bfc81086e374402acf0a629cbc340fdcbc1c47
mirkosertic <mirkosertic@d3b2640a-98bb-4239-95bf-c7bbe60608d9>
2009-12-02 08:40:23 +0000

    Bug Fixing in Naming Pattern interpretation while Reverse Engineering for H2 dialect.

--- trunk/mogwai/MogwaiERDesignerNG/src/test/resources/de/erdesignerng/test/sql/h2/db.sql
+++ trunk/mogwai/MogwaiERDesignerNG/src/test/resources/de/erdesignerng/test/sql/h2/db.sql
@@ -2,9 +2,13 @@ create schema schemaa authorization DBA
 create schema schemab authorization DBA
 set schema schemaa
 create table Table1 (tb1_1 varchar(20) not null,tb1_2 varchar(100) default 'Test',tb1_3 numeric(20,5) not null,CONSTRAINT PK1 PRIMARY KEY (tb1_1))
+create table Table_2 (tb2_1 varchar(20) not null,tb2_2 varchar(100) default 'Test',tb2_3 numeric(20,5) not null,CONSTRAINT PK3 PRIMARY KEY (tb2_1))
+create table Tablea2 (tb3_1 varchar(20) not null,tb3_2 varchar(100) default 'Test',tb3_3 numeric(20,5) not null,CONSTRAINT PK6 PRIMARY KEY (tb3_1))
+alter table Table1 add constraint FK1 foreign key (tb1_1) references Table_2(tb2_1) on delete no action on update no action;
 set schema schemab
 create table Table1 (tb2_1 varchar(20) not null,tb2_2 varchar(100) default 'A' ,tb2_3 numeric(20,5) not null)
 create table Table2 (tb3_1 varchar(20) not null,tb3_2 varchar(100) default 'A' ,tb3_3 numeric(20,5) not null)
+create table Table_5 (tb5_1 varchar(20) not null,tb5_2 varchar(100) default 'Test',tb5_3 numeric(20,5) not null,CONSTRAINT PK8 PRIMARY KEY (tb5_1))
 create view View1 as SELECT * FROM INFORMATION_SCHEMA.SYSTEM_VIEWS 
 create unique index Tabl11_idx1 on Table1 (tb2_2)
 alter table Table2 add constraint PK2 primary key (tb3_1)
