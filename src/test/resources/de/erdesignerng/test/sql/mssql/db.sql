dc478045d8d19a254716108b04de76eecb2ecc19
Mirko Sertic <mirko@mirkosertic.de>
2009-12-02 20:12:56 +0000

    Fixed MSSQL tests

--- src/test/resources/de/erdesignerng/test/sql/mssql/db.sql
+++ src/test/resources/de/erdesignerng/test/sql/mssql/db.sql
@@ -3,9 +3,7 @@ create table Table2 (tb3_1 varchar(20) not null,tb3_2 varchar(100) default 'A' ,
 create table Table_2 (tb2_1 varchar(20) not null,tb2_2 varchar(100) default 'Test',tb2_3 decimal(20,5) not null)
 alter table Table_2 add constraint PK3 primary key (tb2_1)
 create table Tablea2 (tb3_1 varchar(20) not null,tb3_2 varchar(100) default 'Test',tb3_3 decimal(20,5) not null,CONSTRAINT PK6 PRIMARY KEY (tb3_1))
-alter table Tablea2 add constraint PK8 primary key (tb3_1)
-alter table Table2 add constraint PK2 primary key (tb3_1)
-alter table Table1 add constraint FK1 foreign key (tb1_1) references Table_2(tb2_1) on delete no action on update no action;
+alter table Table1 add constraint FK18 foreign key (tb2_1) references Table_2(tb2_1) on delete no action on update no action;
 create view View1 as SELECT * from Table1
 create unique index Tabl11_idx1 on Table1 (tb2_2)
 alter table Table2 add constraint PK2 primary key (tb3_1)
