357bba5ee729c6f8b906246af8f3c5d2e9dfce37
mirkosertic <mirkosertic@d3b2640a-98bb-4239-95bf-c7bbe60608d9>
2009-12-03 18:31:09 +0000

    Fixed MySQL Unit test

--- trunk/mogwai/MogwaiERDesignerNG/src/test/resources/de/erdesignerng/test/sql/mysql/db.sql
+++ trunk/mogwai/MogwaiERDesignerNG/src/test/resources/de/erdesignerng/test/sql/mysql/db.sql
@@ -2,7 +2,7 @@ create table Table1 (tb2_1 varchar(20) not null,tb2_2 varchar(100) default 'A' ,
 create table Table2 (tb3_1 varchar(20) not null,tb3_2 varchar(100) default 'A' ,tb3_3 decimal(20,5) not null)
 create table Table_2 (tb2_1 varchar(20) not null,tb2_2 varchar(100) default 'Test',tb2_3 numeric(20,5) not null,CONSTRAINT PK3 PRIMARY KEY (tb2_1))
 create table Tablea2 (tb3_1 varchar(20) not null,tb3_2 varchar(100) default 'Test',tb3_3 numeric(20,5) not null,CONSTRAINT PK6 PRIMARY KEY (tb3_1))
-alter table Table1 add constraint FK1 foreign key (tb2_1) references Table_2(tb2_1) on delete no action on update no action
+alter table Table1 add constraint FK32 foreign key (tb2_1) references Table_2(tb2_1) on delete no action on update no action
 create view View1 as SELECT * from Table1
 create unique index Tabl11_idx1 on Table1 (tb2_2)
 alter table Table2 add constraint PK2 primary key (tb3_1)
