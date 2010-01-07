29fe16653d5efe7d32795848f8b27e3580669741
Mirko Sertic <mirko.sertic@web.de>
2010-01-07 11:57:41 +0000

    Added additional properties to MSSQL dialect(UI only)

---
+++ src/main/java/de/erdesignerng/dialect/mssql/MSSQLIndexProperties.java
@@ -0,0 +1,52 @@
+/**
+ * Mogwai ERDesigner. Copyright (C) 2002 The Mogwai Project.
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+package de.erdesignerng.dialect.mssql;
+
+import de.erdesignerng.dialect.IndexProperties;
+import de.erdesignerng.model.Index;
+
+public class MSSQLIndexProperties extends IndexProperties {
+
+	public enum IndexTypeEnum {
+		CLUSTERED, NONCLUSTERED;
+	}
+	
+	private IndexTypeEnum indexType;
+	
+	private String fileGroup = "default";
+
+	public MSSQLIndexProperties(Index aIndex) {
+		super(aIndex);
+	}
+
+	public IndexTypeEnum getIndexType() {
+		return indexType;
+	}
+
+	public void setIndexType(IndexTypeEnum indexType) {
+		this.indexType = indexType;
+	}
+
+	public String getFileGroup() {
+		return fileGroup;
+	}
+
+	public void setFileGroup(String fileGroup) {
+		this.fileGroup = fileGroup;
+	}
+}
