885626e1f57350d156324b6780d5c4af1c781fd3
Mirko Sertic <mirko@mirkosertic.de>
2010-04-07 17:20:45 +0000

    Removed support for "DISTINCT" (enhanced basic datatypes) CustomTypes because it is exactly the same as a Domain.
    CustomTypes now can only be java.sql.Types.STRUCT or java.sql.Types.ARRAY.

--- src/main/java/de/erdesignerng/model/serializer/AbstractXMLCustomTypeSerializer.java
+++ src/main/java/de/erdesignerng/model/serializer/AbstractXMLCustomTypeSerializer.java
@@ -27,8 +27,6 @@ public abstract class AbstractXMLCustomTypeSerializer extends CommonAbstractXMLS
 
     protected static final String CUSTOMTYPE = "CustomType";
 
-    protected static final String DISTINCT = "Distinct";
-
     protected static final String STRUCT = "Struct";
 
     protected static final String ARRAY = "Array";
