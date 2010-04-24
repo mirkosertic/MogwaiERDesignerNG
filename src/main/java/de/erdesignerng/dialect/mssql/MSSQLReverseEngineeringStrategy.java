98c61fdcfafbcfafe7b7a303dfb514322b31b246
Mirko Sertic <mirko@mirkosertic.de>
2010-04-24 15:16:55 +0000

    Bug fixing

--- src/main/java/de/erdesignerng/dialect/mssql/MSSQLReverseEngineeringStrategy.java
+++ src/main/java/de/erdesignerng/dialect/mssql/MSSQLReverseEngineeringStrategy.java
@@ -23,58 +23,119 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import de.erdesignerng.ERDesignerBundle;
 import de.erdesignerng.dialect.JDBCReverseEngineeringStrategy;
+import de.erdesignerng.dialect.ReverseEngineeringNotifier;
+import de.erdesignerng.dialect.ReverseEngineeringOptions;
 import de.erdesignerng.dialect.TableEntry;
 import de.erdesignerng.exception.ReverseEngineeringException;
 import de.erdesignerng.model.CascadeType;
+import de.erdesignerng.model.CustomType;
+import de.erdesignerng.model.Model;
 import de.erdesignerng.model.View;
+import de.erdesignerng.modificationtracker.VetoException;
 
 /**
  * @author $Author: mirkosertic $
  * @version $Date: 2009-03-09 19:07:29 $
  */
-public class MSSQLReverseEngineeringStrategy extends JDBCReverseEngineeringStrategy<MSSQLDialect> {
+public class MSSQLReverseEngineeringStrategy extends
+		JDBCReverseEngineeringStrategy<MSSQLDialect> {
 
-    public MSSQLReverseEngineeringStrategy(MSSQLDialect aDialect) {
-        super(aDialect);
-    }
+	public MSSQLReverseEngineeringStrategy(MSSQLDialect aDialect) {
+		super(aDialect);
+	}
 
-    @Override
-    protected boolean isTableTypeView(String aTableType) {
-        return VIEW_TABLE_TYPE.equals(aTableType);
-    }
+	@Override
+	protected boolean isTableTypeView(String aTableType) {
+		return VIEW_TABLE_TYPE.equals(aTableType);
+	}
 
-    @Override
-    protected String reverseEngineerViewSQL(TableEntry aViewEntry, Connection aConnection, View aView)
-            throws SQLException, ReverseEngineeringException {
-        PreparedStatement theStatement = aConnection
-                .prepareStatement("SELECT * FROM INFORMATION_SCHEMA.VIEWS WHERE TABLE_NAME = ?");
-        theStatement.setString(1, aViewEntry.getTableName());
-        ResultSet theResult = null;
-        try {
-            theResult = theStatement.executeQuery();
-            while (theResult.next()) {
-                String theViewDefinition = theResult.getString("VIEW_DEFINITION");
-                theViewDefinition = extractSelectDDLFromViewDefinition(theViewDefinition);
-                return theViewDefinition;
-            }
-            return null;
-        } finally {
-            if (theResult != null) {
-                theResult.close();
-            }
-            theStatement.close();
-        }
-    }
+	@Override
+	protected String reverseEngineerViewSQL(TableEntry aViewEntry,
+			Connection aConnection, View aView) throws SQLException,
+			ReverseEngineeringException {
+		PreparedStatement theStatement = aConnection
+				.prepareStatement("SELECT * FROM INFORMATION_SCHEMA.VIEWS WHERE TABLE_NAME = ?");
+		theStatement.setString(1, aViewEntry.getTableName());
+		ResultSet theResult = null;
+		try {
+			theResult = theStatement.executeQuery();
+			while (theResult.next()) {
+				String theViewDefinition = theResult
+						.getString("VIEW_DEFINITION");
+				theViewDefinition = extractSelectDDLFromViewDefinition(theViewDefinition);
+				return theViewDefinition;
+			}
+			return null;
+		} finally {
+			if (theResult != null) {
+				theResult.close();
+			}
+			theStatement.close();
+		}
+	}
 
-    @Override
-    protected CascadeType getCascadeType(int aValue) {
-        switch (aValue) {
-        case DatabaseMetaData.importedKeyRestrict:
-            // Restrict is not supported my this db
-            aValue = DatabaseMetaData.importedKeyNoAction;
-            break;
-        }
-        return super.getCascadeType(aValue);
-    }
+	@Override
+	protected CascadeType getCascadeType(int aValue) {
+		switch (aValue) {
+		case DatabaseMetaData.importedKeyRestrict:
+			// Restrict is not supported my this db
+			aValue = DatabaseMetaData.importedKeyNoAction;
+			break;
+		}
+		return super.getCascadeType(aValue);
+	}
+
+	@Override
+	protected void reverseEngineerCustomType(Model aModel,
+			CustomType aCustomType, ReverseEngineeringOptions aOptions,
+			ReverseEngineeringNotifier aNotifier, Connection aConnection)
+			throws SQLException {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	protected void reverseEngineerCustomTypes(Model aModel,
+			ReverseEngineeringOptions aOptions,
+			ReverseEngineeringNotifier aNotifier, Connection aConnection)
+			throws SQLException, ReverseEngineeringException {
+		String theQuery = "select b.principal_id,b.name,a.* from sys.types a,sys.schemas b where a.schema_id = b.schema_id and a.is_table_type = 0 and a.is_user_defined = 1";
+		PreparedStatement theStatement = aConnection.prepareStatement(theQuery);
+		ResultSet theResult = null;
+		try {
+			theResult = theStatement.executeQuery();
+			while (theResult.next()) {
+				String theCustomTypeName = theResult.getString("name");
+
+				aNotifier.notifyMessage(ERDesignerBundle.ENGINEERINGCUSTOMTYPE,
+						theCustomTypeName);
+
+				CustomType theCustomType = aModel.getCustomTypes()
+						.findByNameAndSchema(theCustomTypeName, null);
+				if (theCustomType != null) {
+					throw new ReverseEngineeringException(
+							"Duplicate custom datatype found : "
+									+ theCustomTypeName);
+				}
+
+				theCustomType = new CustomType();
+				theCustomType.setName(theCustomTypeName);
+
+				try {
+					aModel.addCustomType(theCustomType);
+				} catch (VetoException e) {
+					throw new ReverseEngineeringException(e.getMessage(), e);
+				}
+
+				reverseEngineerCustomType(aModel, theCustomType, aOptions,
+						aNotifier, aConnection);
+			}
+		} finally {
+			if (theResult != null) {
+				theResult.close();
+			}
+			theStatement.close();
+		}
+	}
 }
