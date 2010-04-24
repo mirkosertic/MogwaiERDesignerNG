e14e23feab04a0a925e02537948422f87ab931ba
mirkosertic <mirkosertic@d3b2640a-98bb-4239-95bf-c7bbe60608d9>
2010-04-24 12:37:39 +0000

    Bug fixing

--- trunk/mogwai/MogwaiERDesignerNG/src/main/java/de/erdesignerng/model/IndexExpressionList.java
+++ trunk/mogwai/MogwaiERDesignerNG/src/main/java/de/erdesignerng/model/IndexExpressionList.java
@@ -17,90 +17,124 @@
  */
 package de.erdesignerng.model;
 
+import org.apache.commons.lang.StringUtils;
+
 import de.erdesignerng.exception.ElementAlreadyExistsException;
 
 public class IndexExpressionList extends ModelItemVector<IndexExpression> {
 
-    /**
-     * Find an index expression by attribute name.
-     * 
-     * @param aAttributeName
-     *            the name of the attribute
-     * @return the expression or null if nothing was found
-     */
-    public IndexExpression findByAttributeName(String aAttributeName) {
-        for (IndexExpression theExpression : this) {
-            Attribute theAttribute = theExpression.getAttributeRef();
-            if (theAttribute != null) {
-                if (aAttributeName.equals(theAttribute.getName())) {
-                    return theExpression;
-                }
-            }
-        }
-        return null;
-    }
+	/**
+	 * Find an index expression by attribute name.
+	 * 
+	 * @param aAttributeName
+	 *            the name of the attribute
+	 * @return the expression or null if nothing was found
+	 */
+	public IndexExpression findByAttributeName(String aAttributeName) {
+		for (IndexExpression theExpression : this) {
+			Attribute theAttribute = theExpression.getAttributeRef();
+			if (theAttribute != null) {
+				if (aAttributeName.equals(theAttribute.getName())) {
+					return theExpression;
+				}
+			}
+		}
+		return null;
+	}
 
-    /**
-     * Find an index expression by is referred attribute.
-     * 
-     * @param aAttribute
-     *            the attribute
-     * @return the expression or null if nothing was found
-     */
-    public IndexExpression findByAttribute(Attribute aAttribute) {
-        for (IndexExpression theExpression : this) {
-            if (aAttribute.equals(theExpression.getAttributeRef())) {
-                return theExpression;
-            }
-        }
-        return null;
-    }
+	/**
+	 * Find an index expression by is referred attribute.
+	 * 
+	 * @param aAttribute
+	 *            the attribute
+	 * @return the expression or null if nothing was found
+	 */
+	public IndexExpression findByAttribute(Attribute aAttribute) {
+		for (IndexExpression theExpression : this) {
+			if (aAttribute.equals(theExpression.getAttributeRef())) {
+				return theExpression;
+			}
+		}
+		return null;
+	}
 
-    /**
-     * Add an expression for an attribute to the list.
-     * 
-     * @param aAttribute
-     *            the attribute
-     * @return the created IndexExpression
-     * @throws ElementAlreadyExistsException
-     *             is thrown is the attribute is already part of this index
-     */
-    public IndexExpression addExpressionFor(Attribute aAttribute) throws ElementAlreadyExistsException {
-        for (IndexExpression theExpression : this) {
-            if (aAttribute.equals(theExpression.getAttributeRef())) {
-                throw new ElementAlreadyExistsException("The attribute is already part of this index");
-            }
-        }
-        IndexExpression theExpression = new IndexExpression();
-        theExpression.setAttributeRef(aAttribute);
-        add(theExpression);
-        return theExpression;
-    }
+	/**
+	 * Add an expression for an attribute to the list.
+	 * 
+	 * @param aAttribute
+	 *            the attribute
+	 * @return the created IndexExpression
+	 * @throws ElementAlreadyExistsException
+	 *             is thrown is the attribute is already part of this index
+	 */
+	public IndexExpression addExpressionFor(Attribute aAttribute)
+			throws ElementAlreadyExistsException {
+		for (IndexExpression theExpression : this) {
+			if (aAttribute.equals(theExpression.getAttributeRef())) {
+				throw new ElementAlreadyExistsException(
+						"The attribute is already part of this index");
+			}
+		}
+		IndexExpression theExpression = new IndexExpression();
+		theExpression.setAttributeRef(aAttribute);
+		add(theExpression);
+		return theExpression;
+	}
 
-    /**
-     * Add an expression for an attribute to the list.
-     * 
-     * @param aColumnExpression
-     *            the expression
-     * @return the created IndexExpression
-     */
-    public IndexExpression addExpressionFor(String aColumnExpression) {
-        IndexExpression theExpression = new IndexExpression();
-        theExpression.setExpression(aColumnExpression);
-        add(theExpression);
-        return theExpression;
-    }
+	/**
+	 * Add an expression for an attribute to the list.
+	 * 
+	 * @param aColumnExpression
+	 *            the expression
+	 * @return the created IndexExpression
+	 */
+	public IndexExpression addExpressionFor(String aColumnExpression) {
+		IndexExpression theExpression = new IndexExpression();
+		theExpression.setExpression(aColumnExpression);
+		add(theExpression);
+		return theExpression;
+	}
 
-    /**
-     * Remove an attribute from the expression list.
-     * 
-     * @param aAttribute
-     *            the attribute to remove
-     */
-    public void removeAttribute(Attribute aAttribute) {
-        IndexExpression theExpression = findByAttribute(aAttribute);
-        if (theExpression != null) {
-            remove(theExpression);
-        }
-    }
+	/**
+	 * Remove an attribute from the expression list.
+	 * 
+	 * @param aAttribute
+	 *            the attribute to remove
+	 */
+	public void removeAttribute(Attribute aAttribute) {
+		IndexExpression theExpression = findByAttribute(aAttribute);
+		if (theExpression != null) {
+			remove(theExpression);
+		}
+	}
+
+	/**
+	 * Check if this list contails the same expressions as another list.
+	 * 
+	 * @param aExpressions
+	 *            the other list
+	 * @return true if yes, else false
+	 */
+	public boolean containsAllExpressions(IndexExpressionList aExpressions) {
+		if (size() == aExpressions.size()) {
+			for (int i = 0; i < size(); i++) {
+				IndexExpression theA = get(i);
+				IndexExpression theB = aExpressions.get(i);
+
+				if (theA.getAttributeRef() != null) {
+					if (theA.getAttributeRef() != theB.getAttributeRef()) {
+						return false;
+					}
+				}
+				if (theA.getExpression() != null) {
+					if (!StringUtils.equals(theA.getExpression(), theB
+							.getExpression())) {
+						return false;
+					}
+				}
+			}
+			return true;
+		}
+		return false;
+	}
 }
