cdab1cde7f470462cd67a9bbfa6c4ee4c91addd3
mirkosertic <mirkosertic@d3b2640a-98bb-4239-95bf-c7bbe60608d9>
2010-07-14 18:37:53 +0000

    Removed RE for Custom Types for Postgres, as there is an unresolved issue...

--- trunk/mogwai/MogwaiERDesignerNG/src/main/java/de/erdesignerng/dialect/postgres/PostgresDialect.java
+++ trunk/mogwai/MogwaiERDesignerNG/src/main/java/de/erdesignerng/dialect/postgres/PostgresDialect.java
@@ -30,120 +30,129 @@ import de.erdesignerng.dialect.sql92.SQL92Dialect;
  */
 public class PostgresDialect extends SQL92Dialect {
 
-    public PostgresDialect() {
-        setSpacesAllowedInObjectNames(true);
-        setCaseSensitive(false);
-        setMaxObjectNameLength(255);
-        setNullablePrimaryKeyAllowed(false);
-        setCastType(NameCastType.NOTHING);
-        setSupportsDomains(true);
-        setSupportsCustomTypes(true);
-        setSupportsSchemaInformation(true);
-        addSystemSchema("information_schema");
-        addSystemSchema("pg_catalog");
-        addSystemSchema("pg_toast");
-        addSystemSchema("pg_toast_temp");
-        addSystemSchema("pg_toast_temp_1");
-        addSystemSchema("pg_temp_1");
+	public PostgresDialect() {
+		setSpacesAllowedInObjectNames(true);
+		setCaseSensitive(false);
+		setMaxObjectNameLength(255);
+		setNullablePrimaryKeyAllowed(false);
+		setCastType(NameCastType.NOTHING);
+		setSupportsDomains(true);
+		setSupportsCustomTypes(true);
+		setSupportsSchemaInformation(true);
+		addSystemSchema("information_schema");
+		addSystemSchema("pg_catalog");
+		addSystemSchema("pg_toast");
+		addSystemSchema("pg_toast_temp");
+		addSystemSchema("pg_toast_temp_1");
+		addSystemSchema("pg_temp_1");
 
-        registerType(createDataType("bool", "", Types.BIT));
-        registerType(createDataType("bytea", "", Types.BINARY, Types.VARBINARY));
-        registerType(createDataType("name", "", Types.VARCHAR));
-        registerType(createDataType("int8", "", Types.BIGINT));
-        registerType(createDataType("bigserial", "", true, Types.BIGINT));
-        registerType(createDataType("int2", "", Types.SMALLINT, Types.TINYINT));
-        registerType(createDataType("int4", "", Types.INTEGER));
-        registerType(createDataType("serial", "", true, Types.INTEGER));
-        registerType(createDataType("text", "", Types.VARCHAR));
-        registerType(createDataType("oid", "", Types.INTEGER));
-        registerType(createDataType("float4", "", Types.REAL));
-        registerType(createDataType("float8", "", Types.DOUBLE, Types.FLOAT));
-        registerType(createDataType("money", "", Types.DOUBLE));
-        registerType(createDataType("bpchar", "", Types.CHAR));
-        registerType(createDataType("varchar", "[" + GenericDataTypeImpl.SIZE_TOKEN + "]", Types.VARCHAR));
-        registerType(createDataType("date", "", Types.DATE));
-        registerType(createDataType("time", "", Types.TIME));
-        registerType(createDataType("timestamp", "", Types.TIMESTAMP));
-        registerType(createDataType("timestamptz", "", Types.TIMESTAMP));
-        registerType(createDataType("timestamp with time zone", "", Types.TIMESTAMP));
-        registerType(createDataType("timetz", "", Types.TIME));
-        registerType(createDataType("bit", "", Types.BIT));
-        registerType(createDataType("numeric", GenericDataTypeImpl.SIZE_TOKEN + ","
-                + GenericDataTypeImpl.FRACTION_TOKEN, Types.NUMERIC, Types.DECIMAL));
+		registerType(createDataType("bool", "", Types.BIT));
+		registerType(createDataType("bytea", "", Types.BINARY, Types.VARBINARY));
+		registerType(createDataType("name", "", Types.VARCHAR));
+		registerType(createDataType("int8", "", Types.BIGINT));
+		registerType(createDataType("bigserial", "", true, Types.BIGINT));
+		registerType(createDataType("int2", "", Types.SMALLINT, Types.TINYINT));
+		registerType(createDataType("int4", "", Types.INTEGER));
+		registerType(createDataType("serial", "", true, Types.INTEGER));
+		registerType(createDataType("text", "", Types.VARCHAR));
+		registerType(createDataType("oid", "", Types.INTEGER));
+		registerType(createDataType("float4", "", Types.REAL));
+		registerType(createDataType("float8", "", Types.DOUBLE, Types.FLOAT));
+		registerType(createDataType("money", "", Types.DOUBLE));
+		registerType(createDataType("bpchar", "", Types.CHAR));
+		registerType(createDataType("varchar", "["
+				+ GenericDataTypeImpl.SIZE_TOKEN + "]", Types.VARCHAR));
+		registerType(createDataType("date", "", Types.DATE));
+		registerType(createDataType("time", "", Types.TIME));
+		registerType(createDataType("timestamp", "", Types.TIMESTAMP));
+		registerType(createDataType("timestamptz", "", Types.TIMESTAMP));
+		registerType(createDataType("timestamp with time zone", "",
+				Types.TIMESTAMP));
+		registerType(createDataType("timetz", "", Types.TIME));
+		registerType(createDataType("bit", "", Types.BIT));
+		registerType(createDataType("numeric", GenericDataTypeImpl.SIZE_TOKEN
+				+ "," + GenericDataTypeImpl.FRACTION_TOKEN, Types.NUMERIC,
+				Types.DECIMAL));
 
-        // Patch [ 2124875 ] Add Postgres data types
-        registerType(createDataType("char", GenericDataTypeImpl.SIZE_TOKEN, Types.CHAR));
-        registerType(createDataType("character", GenericDataTypeImpl.SIZE_TOKEN, Types.CHAR));
-        registerType(createDataType("character varying", "[" + GenericDataTypeImpl.SIZE_TOKEN + "]", Types.VARCHAR));
-        registerType(createDataType("boolean", "", Types.BOOLEAN));
-        registerType(createDataType("interval", "", Types.TIMESTAMP));
-        registerType(createDataType("smallint", "", Types.SMALLINT));
-        registerType(createDataType("integer", "", Types.INTEGER));
+		// Patch [ 2124875 ] Add Postgres data types
+		registerType(createDataType("char", GenericDataTypeImpl.SIZE_TOKEN,
+				Types.CHAR));
+		registerType(createDataType("character",
+				GenericDataTypeImpl.SIZE_TOKEN, Types.CHAR));
+		registerType(createDataType("character varying", "["
+				+ GenericDataTypeImpl.SIZE_TOKEN + "]", Types.VARCHAR));
+		registerType(createDataType("boolean", "", Types.BOOLEAN));
+		registerType(createDataType("interval", "", Types.TIMESTAMP));
+		registerType(createDataType("smallint", "", Types.SMALLINT));
+		registerType(createDataType("integer", "", Types.INTEGER));
 
-        registerType(createDataType("bigint", "", Types.BIGINT));
-        registerType(createDataType("real", "", Types.REAL));
-        registerType(createDataType("double precision", "", Types.DOUBLE));
-        registerType(createDataType("xml", "", Types.SQLXML));
-        registerType(createDataType("blob", "", Types.BLOB, Types.LONGVARBINARY));
-        registerType(createDataType("clob", "", Types.CLOB, Types.LONGVARCHAR));
+		registerType(createDataType("bigint", "", Types.BIGINT));
+		registerType(createDataType("real", "", Types.REAL));
+		registerType(createDataType("double precision", "", Types.DOUBLE));
+		registerType(createDataType("xml", "", Types.SQLXML));
+		registerType(createDataType("blob", "", Types.BLOB, Types.LONGVARBINARY));
+		registerType(createDataType("clob", "", Types.CLOB, Types.LONGVARCHAR));
 
-        // Patch [ 2874576 ] Reverse-Engineering unterstuetzt INET Datentyp
-        // nicht
-        // @see
-        // http://www.postgresql.org/docs/8.4/interactive/datatype-net-types.html#DATATYPE-INET
-        registerType(createDataType("inet", "", Types.VARCHAR, Types.NVARCHAR));
+		// Patch [ 2874576 ] Reverse-Engineering unterstuetzt INET Datentyp
+		// nicht
+		// @see
+		// http://www.postgresql.org/docs/8.4/interactive/datatype-net-types.html#DATATYPE-INET
+		registerType(createDataType("inet", "", Types.VARCHAR, Types.NVARCHAR));
 
-        // Bug Fixing 2933615 [ERDesignerNG] UUID Datatype not Supported in PostgreSQL
-        registerType(createDataType("uuid", "", Types.VARCHAR, Types.NVARCHAR));
+		// Bug Fixing 2933615 [ERDesignerNG] UUID Datatype not Supported in
+		// PostgreSQL
+		registerType(createDataType("uuid", "", Types.VARCHAR, Types.NVARCHAR));
 
-        // TODO [dr-death] add all missing datatypes according to
-        // @see
-        // http://www.postgresql.org/docs/8.4/interactive/datatype.html#DATATYPE-TABLE
-        // registerType(createDataType("_point", "", Types.OTHER));
-        // registerType(createDataType("point", "", Types.OTHER));
-        // registerType(createDataType("abstime", "", Types.OTHER));
-        // registerType(createDataType("_xml", "", Types.OTHER));
+		// TODO [dr-death] add all missing datatypes according to
+		// @see
+		// http://www.postgresql.org/docs/8.4/interactive/datatype.html#DATATYPE-TABLE
+		// registerType(createDataType("_point", "", Types.OTHER));
+		// registerType(createDataType("point", "", Types.OTHER));
+		// registerType(createDataType("abstime", "", Types.OTHER));
+		// registerType(createDataType("_xml", "", Types.OTHER));
 
-        seal();
-    }
+		seal();
+	}
 
-    @Override
-    public PostgresReverseEngineeringStrategy getReverseEngineeringStrategy() {
-        return new PostgresReverseEngineeringStrategy(this);
-    }
+	@Override
+	public PostgresReverseEngineeringStrategy getReverseEngineeringStrategy() {
+		return new PostgresReverseEngineeringStrategy(this);
+	}
 
-    @Override
-    public String getUniqueName() {
-        return "PostgresDialect";
-    }
+	@Override
+	public String getUniqueName() {
+		return "PostgresDialect";
+	}
 
-    @Override
-    public String getDriverClassName() {
-        return "org.postgresql.Driver";
-    }
+	@Override
+	public String getDriverClassName() {
+		return "org.postgresql.Driver";
+	}
 
-    @Override
-    public String getDriverURLTemplate() {
-        return "jdbc:postgresql://<host>:<port>/<db>";
-    }
+	@Override
+	public String getDriverURLTemplate() {
+		return "jdbc:postgresql://<host>:<port>/<db>";
+	}
 
-    @Override
-    public PostgresSQLGenerator createSQLGenerator() {
-        return new PostgresSQLGenerator(this);
-    }
+	@Override
+	public PostgresSQLGenerator createSQLGenerator() {
+		return new PostgresSQLGenerator(this);
+	}
 
-    @Override
-    public Class getHibernateDialectClass() {
-        return org.hibernate.dialect.PostgreSQLDialect.class;
-    }
+	@Override
+	public Class getHibernateDialectClass() {
+		return org.hibernate.dialect.PostgreSQLDialect.class;
+	}
 
-    @Override
-    public DataType createDataType(String aName, String aDefinition, int... aJdbcType) {
-        return new PostgresDataType(aName, aDefinition, aJdbcType);
-    }
+	@Override
+	public DataType createDataType(String aName, String aDefinition,
+			int... aJdbcType) {
+		return new PostgresDataType(aName, aDefinition, aJdbcType);
+	}
 
-    @Override
-    public DataType createDataType(String aName, String aDefinition, boolean aIdentity, int... aJdbcType) {
-        return new PostgresDataType(aName, aDefinition, aIdentity, aJdbcType);
-    }
+	@Override
+	public DataType createDataType(String aName, String aDefinition,
+			boolean aIdentity, int... aJdbcType) {
+		return new PostgresDataType(aName, aDefinition, aIdentity, aJdbcType);
+	}
 }
