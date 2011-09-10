7accbc5fb951e9410033f410d215abd4368fd20f
Mirko Sertic <mirko@mirkosertic.de>
2011-09-10 14:45:27 +0000

    Release 3.0.0-M1

--- run64.bat
+++ run64.bat
@@ -1,2 +1,2 @@
 @echo off
-java -Djava.library.path=.\java3d\win32 -cp .\lib\mogwai-smartstart-1.0.jar de.mogwai.smartstart.SmartStart de.erdesignerng.visual.ERDesigner lib
+java -Djava.library.path=.\java3d\win64 -cp .\lib\mogwai-smartstart-1.1.jar de.mogwai.smartstart.SmartStart de.erdesignerng.visual.ERDesigner lib
