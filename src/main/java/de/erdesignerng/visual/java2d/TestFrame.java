b450d0992c7a925892813081b28c34111dea372f
Mirko Sertic <mirko@mirkosertic.de>
2011-09-11 19:46:42 +0000

    Working on 2D editor

--- src/main/java/de/erdesignerng/visual/java2d/TestFrame.java
+++ src/main/java/de/erdesignerng/visual/java2d/TestFrame.java
@@ -33,71 +33,94 @@ import org.xml.sax.SAXException;
 public class TestFrame extends JFrame {
 
     private Model model;
+    private EditorPanel panel;
 
     public TestFrame() throws ParserConfigurationException, IOException, SAXException {
         InputStream theStream = new FileInputStream("D:\\Temp\\Capitastra_6_5_0.mxm");
         model = ModelIOUtilities.getInstance().deserializeModelFromXML(theStream);
+
+        panel = new EditorPanel() {
+            @Override
+            public void componentClicked(EditorComponent aComponent) {
+                generateGraphFor((Table) aComponent.userObject);
+            }
+        };
+        panel.setBackground(Color.black);
+        setContentPane(panel);
+
+        Table theTable = model.getTables().findByName("CAPITIMEPOINT");
+        theTable = model.getTables().findByName("GRUNDSTUECK");
+        generateGraphFor(theTable);
     }
 
-    public void generateGraphFor(String aTableName) {
+    public void generateGraphFor(Table aTable) {
 
-        EditorPanel thePanel = new EditorPanel();
-        thePanel.setBackground(Color.black);
-        setContentPane(thePanel);
+        panel.cleanup();
 
         Map<ModelItem, EditorPanel.EditorComponent> theComponentMap = new HashMap<ModelItem, EditorPanel.EditorComponent>();
 
-        Table theTable = model.getTables().findByName(aTableName);
-        TableComponent theButton = new TableComponent(theTable);
-        EditorPanel.EditorComponent theRoot = new EditorPanel.EditorComponent(0, 0, theButton, true);
-        thePanel.add(theRoot);
+        TableComponent theButton = new TableComponent(aTable);
+        EditorPanel.EditorComponent theRoot = new EditorPanel.EditorComponent(aTable, 0, 0, theButton, true);
+        panel.add(theRoot);
 
-        theComponentMap.put(theTable, theRoot);
+        theComponentMap.put(aTable, theRoot);
 
         int r1 = 250;
 
         List<Table> theAlreadyKnown = new ArrayList<Table>();
-        theAlreadyKnown.add(theTable);
+        theAlreadyKnown.add(aTable);
 
         // Level 1
-        List<Relation> theIncomingRelationsLevel1 = model.getRelations().getForeignKeysFor(theTable);
+        List<Relation> theIncomingRelationsLevel1 = model.getRelations().getForeignKeysFor(aTable);
+        //List<Relation> theOutgoingRelationsLevel1 = model.getRelations().getExportedKeysFor(aTable);
+        //theIncomingRelationsLevel1.addAll(theOutgoingRelationsLevel1);
         List<Table> theTablesLevel1 = new ArrayList<Table>();
         for (Relation theRelation : theIncomingRelationsLevel1) {
             if (!theTablesLevel1.contains(theRelation.getExportingTable()) && !theAlreadyKnown.contains(theRelation.getExportingTable())) {
                 theTablesLevel1.add(theRelation.getExportingTable());
                 theAlreadyKnown.add(theRelation.getExportingTable());
             }
+            if (!theTablesLevel1.contains(theRelation.getImportingTable()) && !theAlreadyKnown.contains(theRelation.getImportingTable())) {
+                theTablesLevel1.add(theRelation.getImportingTable());
+                theAlreadyKnown.add(theRelation.getImportingTable());
+            }
         }
-        float theIncrement1 = 360 / theTablesLevel1.size();
-        float theAngleLevel1 = 0;
 
-        for (Table theTableLevel1 : theTablesLevel1) {
+        if (theTablesLevel1.size() > 0) {
+            float theIncrement1 = 360 / theTablesLevel1.size();
+            float theAngleLevel1 = 0;
 
-            TableComponent theButtonLevel1 = new TableComponent(theTableLevel1);
-            EditorPanel.EditorComponent theChildLevel1 = new EditorPanel.EditorComponent(theAngleLevel1, r1, theButtonLevel1);
-            thePanel.add(theChildLevel1);
+            for (Table theTableLevel1 : theTablesLevel1) {
 
-            theComponentMap.put(theTableLevel1, theChildLevel1);
+                TableComponent theButtonLevel1 = new TableComponent(theTableLevel1);
+                EditorPanel.EditorComponent theChildLevel1 = new EditorPanel.EditorComponent(theTableLevel1, theAngleLevel1, r1, theButtonLevel1);
+                panel.add(theChildLevel1);
 
-            theAngleLevel1 += theIncrement1;
+                theComponentMap.put(theTableLevel1, theChildLevel1);
+
+                theAngleLevel1 += theIncrement1;
+            }
         }
 
         for (Table theKnownTable : theAlreadyKnown) {
             for (Relation theRelation : model.getRelations().getForeignKeysFor(theKnownTable)) {
                 EditorPanel.EditorComponent theFrom = theComponentMap.get(theRelation.getExportingTable());
                 EditorPanel.EditorComponent theTo = theComponentMap.get(theRelation.getImportingTable());
-                if (theFrom != null && theTo!= null && theFrom != theTo && !thePanel.hasConnection(theFrom, theTo)) {
-                    thePanel.add(new EditorPanel.Connector(theFrom, theTo));
+                if (theFrom != null && theTo != null && theFrom != theTo && !panel.hasConnection(theFrom, theTo)) {
+                    panel.add(new EditorPanel.Connector(theFrom, theTo));
                 }
             }
         }
+
+        panel.invalidate();
+        panel.repaint();
+        panel.explodeAnimation();
     }
 
     public static void main(String[] args) throws IOException, SAXException, ParserConfigurationException {
         TestFrame theFrame = new TestFrame();
         theFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         theFrame.setSize(800, 600);
-        theFrame.generateGraphFor("CAPITIMEPOINT");
         theFrame.setVisible(true);
     }
 }
