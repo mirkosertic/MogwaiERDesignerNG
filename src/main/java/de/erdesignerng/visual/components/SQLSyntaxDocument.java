0e8bc9a36b56eda5a056c5aaf8088de2e9b1d352
Mirko Sertic <mirko.sertic@web.de>
2011-09-21 19:01:37 +0000

    Added missing SQL keywords and functions

--- src/main/java/de/erdesignerng/visual/components/SQLSyntaxDocument.java
+++ src/main/java/de/erdesignerng/visual/components/SQLSyntaxDocument.java
@@ -17,13 +17,8 @@
  */
 package de.erdesignerng.visual.components;
 
-import javax.swing.text.AttributeSet;
-import javax.swing.text.BadLocationException;
-import javax.swing.text.DefaultStyledDocument;
-import javax.swing.text.Element;
-import javax.swing.text.SimpleAttributeSet;
-import javax.swing.text.StyleConstants;
-import java.awt.*;
+import java.awt.Color;
+import javax.swing.text.*;
 
 /**
  * Document highlighting Oracle PL/SQL syntax.
@@ -60,8 +55,8 @@ class SQLSyntaxDocument extends DefaultStyledDocument {
     public SQLSyntaxDocument() {
         root = this.getDefaultRootElement();
 
-        scanner.setKeywords(OracleKeywords);
-        scanner.setBuiltinFunctions(OracleFunctions);
+        scanner.setKeywords(SQLKEYWORDS);
+        scanner.setBuiltinFunctions(SQLFUNCTIONS);
 
         StyleConstants.setForeground(
                 sas[SQLScanner.TOKEN_STRING] = new SimpleAttributeSet(),
@@ -85,7 +80,8 @@ class SQLSyntaxDocument extends DefaultStyledDocument {
         sas[SQLScanner.TOKEN_WHITESPACE] = new SimpleAttributeSet();
     }
 
-    public final String[] OracleKeywords = {"UNIQUE", "ADD", "SELECT", "FROM", "WHERE",
+    // There are a lot of duplicates, as this code was generated from JDBC DatabaseMetaData
+    public final String[] SQLKEYWORDS = {"UNIQUE", "ADD", "SELECT", "FROM", "WHERE",
             "UPDATE", "SET", "INSERT", "INTO", "CREATE", "REPLACE", "VALUES",
             "DROP", "CASCADE", "DELETE", "PROCEDURE", "FUNCTION", "TABLE",
             "VIEW", "PACKAGE", "BODY", "TRIGGER", "SEQUENCE", "AND", "OR",
@@ -106,9 +102,46 @@ class SQLSyntaxDocument extends DefaultStyledDocument {
             "BULK", "COLLECT", "CLOB", "CLOSE", "RAW", "ROWID", "NCLOB",
             "NCHAR", "UROWID", "RECORD", "VARRAY", "VARCHAR2", "DEC",
             "DECIMAL", "DOUBLE", "PRECISION", "FLOAT", "INTEGER", "INT",
-            "NUMERIC", "REAL", "SMALLINT", "PLS_INTEGER", "USING", "CONSTRAINT"};
+            "NUMERIC", "REAL", "SMALLINT", "PLS_INTEGER", "USING", "CONSTRAINT",
+            "LIMIT", "MINUS", "ROWNUM", "SYSDATE", "SYSTIME", "SYSTIMESTAMP", "TODAY",
+            "ARITH_OVERFLOW", "BREAK", "BROWSE", "BULK", "CHAR_CONVERT", "CHECKPOINT",
+            "CLUSTERED", "COMPUTE", "CONFIRM", "CONTROLROW", "DATA_PGS", "DATABASE",
+            "DBCC", "DISK", "DUMMY", "DUMP", "ENDTRAN", "ERRLVL", "ERRORDATA", "ERROREXIT",
+            "EXIT", "FILLFACTOR", "HOLDLOCK", "IDENTITY_INSERT", "IF", "INDEX", "KILL",
+            "LINENO", "LOAD", "MAX_ROWS_PER_PAGE", "MIRROR", "MIRROREXIT", "NOHOLDLOCK",
+            "NONCLUSTERED", "NUMERIC_TRUNCATION", "OFF", "OFFSETS", "ONCE,ONLINE", "OVER",
+            "PARTITION", "PERM", "PERMANENT", "PLAN", "PRINT", "PROC", "PROCESSEXIT", "RAISERROR",
+            "READ", "READTEXT", "RECONFIGURE", "REPLACE", "RESERVED_PGS", "RETURN", "ROLE",
+            "ROWCNT", "ROWCOUNT", "RULE", "SAVE", "SETUSER", "SHARED", "SHUTDOWN", "SOME",
+            "STATISTICS", "STRIPE", "SYB_IDENTITY", "SYB_RESTREE", "SYB_TERMINATE", "TEMP",
+            "TEXTSIZE", "TRAN", "TRIGGER", "TRUNCATE", "TSEQUAL,UNPARTITION", "USE", "USED_PGS",
+            "USER_OPTION", "WAITFOR", "WHILE", "WRITETEXT", "ACCESS", "ADD", "ALTER", "AUDIT",
+            "CLUSTER", "COLUMN", "COMMENT", "COMPRESS", "CONNECT", "DATE", "DROP", "EXCLUSIVE",
+            "FILE", "IDENTIFIED", "IMMEDIATE", "INCREMENT", "INDEX", "INITIAL", "INTERSECT",
+            "LEVEL", "LOCK", "LONG", "MAXEXTENTS", "MINUS", "MODE", "NOAUDIT", "NOCOMPRESS",
+            "NOWAIT", "NUMBER", "OFFLINE", "ONLINE", "PCTFREE", "PRIOR", "abort", "acl", "add",
+            "aggregate", "append", "archive", "arch_store2", "backward", "binary", "boolean", "change",
+            "cluster", "copy", "database", "delimiter", "delimiters", "do", "extend", "explain", "forward",
+            "heavy", "index", "inherits", "isnull", "light", "listen", "load", "merge", "nothing", "notify"
+            , "notnull", "oids", "purge", "rename", "replace", "retrieve", "returns", "rule", "recipe", "setof",
+            "stdin", "stdout", "store", "vacuum", "verbose", "version", "ACCESSIBLE", "ANALYZE", "ASENSITIVE",
+            "BEFORE", "BIGINT", "BINARY", "BLOB", "CALL", "CHANGE", "CONDITION", "DATABASE", "DATABASES", "DAY_HOUR",
+            "DAY_MICROSECOND", "DAY_MINUTE", "DAY_SECOND", "DELAYED", "DETERMINISTIC", "DISTINCTROW", "DIV", "DUAL",
+            "EACH", "ELSEIF", "ENCLOSED", "ESCAPED", "EXIT", "EXPLAIN", "FLOAT4", "FLOAT8", "FORCE", "FULLTEXT", "HIGH_PRIORITY",
+            "HOUR_MICROSECOND", "HOUR_MINUTE", "HOUR_SECOND", "IF", "IGNORE", "INFILE", "INOUT", "INT1", "INT2", "INT3",
+            "INT4", "INT8", "ITERATE", "KEYS", "KILL", "LEAVE", "LIMIT", "LINEAR", "LINES", "LOAD",
+            "LOCALTIME", "LOCALTIMESTAMP", "LOCK", "LONG", "LONGBLOB", "LONGTEXT", "LOOP", "LOW_PRIORITY",
+            "MEDIUMBLOB", "MEDIUMINT", "MEDIUMTEXT", "MIDDLEINT", "MINUTE_MICROSECOND", "MINUTE_SECOND",
+            "MOD,MODIFIES", "NO_WRITE_TO_BINLOG", "OPTIMIZE", "OPTIONALLY", "OUT", "OUTFILE",
+            "PURGE", "RANGE", "READS", "READ_ONLY", "READ_WRITE", "REGEXP,RELEASE", "RENAME", "REPEAT",
+            "REPLACE", "REQUIRE", "RETURN", "RLIKE", "SCHEMAS", "SECOND_MICROSECOND", "SENSITIVE",
+            "SEPARATOR", "SHOW", "SPATIAL", "SPECIFIC", "SQLEXCEPTION", "SQL_BIG_RESULT", "SQL_CALC_FOUND_ROWS",
+            "SQL_SMALL_RESULT", "SSL", "STARTING", "STRAIGHT_JOIN", "TERMINATED", "TINYBLOB", "TINYINT", "TINYTEXT",
+            "TRIGGER", "UNDO", "UNLOCK", "UNSIGNED", "USE", "UTC_DATE", "UTC_TIME", "UTC_TIMESTAMP",
+            "VARBINARY", "VARCHARACTER", "WHILE", "X509", "XOR", "YEAR_MONTH", "ZEROFILL", "INNER", "OUTER", "JOIN"};
 
-    public final String[] OracleFunctions = {"ABS", "ACOS", "ADD_MONTHS",
+    // There are a lot of duplicates, as this code was generated from JDBC DatabaseMetaData
+    public final String[] SQLFUNCTIONS = {"ABS", "ACOS", "ADD_MONTHS",
             "ATAN", "ATAN2", "CEIL", "COS", "COSH", "EXP", "FLOOR", "LN",
             "LOG", "MOD", "POWER", "ROUND", "SIGN", "SIN", "SINH", "SQRT",
             "TAN", "TANH", "CHR", "CONCAT", "INITCAP", "LOWER", "LPAD",
@@ -124,7 +157,62 @@ class SQLSyntaxDocument extends DefaultStyledDocument {
             "NLS_CHARSET_NAME", "NVL", "SYS_CONTEXT", "SYS_GUID", "UID",
             "USER", "USERENV", "VSIZE", "DEREF", "MAKE_REF", "REF", "REFTOHEX",
             "VALUE", "AVG", "COUNT", "GROUPING", "MAX", "MIN", "STDDEV", "SUM",
-            "VARIANCE"};
+            "VARIANCE", "ABS", "ACOS", "ASIN", "ATAN", "COS", "COT", "SIN", "TAN",
+            "ATAN2", "BITAND", "BITOR", "BITXOR", "MOD", "CEILING", "DEGREES", "EXP",
+            "FLOOR", "LOG", "LOG10", "RADIANS", "SQRT", "PI", "POWER", "RAND",
+            "RANDOM_UUID", "ROUND", "ROUNDMAGIC", "SECURE_RAND", "SIGN", "ENCRYPT",
+            "DECRYPT", "HASH", "TRUNCATE", "COMPRESS", "EXPAND", "ZERO", "ASCII",
+            "BIT_LENGTH", "LENGTH", "OCTET_LENGTH", "CHAR", "CONCAT", "DIFFERENCE",
+            "HEXTORAW", "RAWTOHEX", "INSTR", "INSERT", "LOWER", "UPPER", "LEFT",
+            "RIGHT", "LOCATE", "POSITION", "LPAD", "RPAD", "LTRIM", "RTRIM",
+            "TRIM", "REGEXP_REPLACE", "REPEAT", "REPLACE", "SOUNDEX", "SPACE",
+            "STRINGDECODE", "STRINGENCODE", "STRINGTOUTF8", "SUBSTRING",
+            "UTF8TOSTRING", "XMLATTR", "XMLNODE", "XMLCOMMENT", "XMLCDATA",
+            "XMLSTARTDOC", "XMLTEXT", "CURRENT_DATE", "CURRENT_TIME",
+            "CURRENT_TIMESTAMP", "DATEADD", "DATEDIFF", "DAYNAME", "DAY_OF_MONTH",
+            "DAY_OF_WEEK", "DAY_OF_YEAR", "EXTRACT", "FORMATDATETIME", "HOUR",
+            "MINUTE", "MONTH", "MONTHNAME", "PARSEDATETIME", "QUARTER", "SECOND", "WEEK",
+            "YEAR", "ABS", "ACOS", "ASIN", "ATAN", "ATAN2", "BITAND", "BITOR", "BITXOR",
+            "CEILING", "COS", "COT", "DEGREES", "EXP", "FLOOR", "LOG", "LOG10", "MOD",
+            "PI", "POWER", "RADIANS", "RAND", "ROUND", "ROUNDMAGIC", "SIGN", "SIN",
+            "SQRT", "TAN", "TRUNCATE", "ASCII", "CHAR", "CONCAT", "DIFFERENCE",
+            "HEXTORAW", "INSERT", "LCASE", "LEFT", "LENGTH", "LOCATE", "LTRIM",
+            "RAWTOHEX", "REPEAT", "REPLACE", "RIGHT", "RTRIM", "SOUNDEX", "SPACE",
+            "SUBSTR", "UCASE", "CURDATE", "CURTIME", "DATEDIFF", "DAYNAME", "DAYOFMONTH",
+            "DAYOFWEEK", "DAYOFYEAR", "HOUR", "MINUTE", "MONTH", "MONTHNAME",
+            "NOW", "QUARTER", "SECOND", "SECONDS_SINCE_MIDNIGHT", "TIMESTAMPADD",
+            "TIMESTAMPDIFF", "TO_CHAR", "WEEK", "YEAR", "abs", "acos", "asin", "atan",
+            "atan2", "ceiling", "cos", "cot", "degrees", "exp", "floor", "log", "log10",
+            "mod", "pi", "power", "radians", "rand", "round", "sign", "sin", "sqrt", "tan",
+            "ascii", "char", "concat", "difference", "insert", "lcase", "left", "length",
+            "locate", "ltrim", "repeat", "replace", "right", "rtrim", "soundex", "space",
+            "substring", "ucase", "curdate", "curtime", "dayname", "dayofmonth", "dayofweek",
+            "dayofyear", "hour", "minute", "month", "monthname", "now", "quarter",
+            "timestampadd", "timestampdiff", "second", "week", "year", "ABS", "ACOS", "ASIN",
+            "ATAN", "ATAN2", "BIT_COUNT", "CEILING", "COS", "COT", "DEGREES", "EXP", "FLOOR",
+            "LOG", "LOG10", "MAX", "MIN", "MOD", "PI", "POW", "POWER", "RADIANS", "RAND",
+            "ROUND", "SIN", "SQRT", "TAN", "TRUNCATE", "ASCII", "BIN", "BIT_LENGTH", "CHAR",
+            "CHARACTER_LENGTH", "CHAR_LENGTH", "CONCAT", "CONCAT_WS", "CONV", "ELT", "EXPORT_SET",
+            "FIELD", "FIND_IN_SET", "HEX", "INSERT", "INSTR", "LCASE", "LEFT", "LENGTH",
+            "LOAD_FILE", "LOCATE", "LOCATE", "LOWER", "LPAD", "LTRIM", "MAKE_SET",
+            "MATCH", "MID", "OCT", "OCTET_LENGTH", "ORD", "POSITION", "QUOTE", "REPEAT",
+            "REPLACE", "REVERSE", "RIGHT", "RPAD", "RTRIM", "SOUNDEX", "SPACE", "STRCMP",
+            "SUBSTRING", "SUBSTRING", "SUBSTRING", "SUBSTRING", "SUBSTRING_INDEX",
+            "TRIM", "UCASE", "UPPER", "DAYOFWEEK", "WEEKDAY", "DAYOFMONTH", "DAYOFYEAR",
+            "MONTH", "DAYNAME", "MONTHNAME", "QUARTER", "WEEK", "YEAR", "HOUR", "MINUTE",
+            "SECOND", "PERIOD_ADD", "PERIOD_DIFF", "TO_DAYS", "FROM_DAYS", "DATE_FORMAT",
+            "TIME_FORMAT", "CURDATE", "CURRENT_DATE", "CURTIME", "CURRENT_TIME", "NOW",
+            "SYSDATE", "CURRENT_TIMESTAMP", "UNIX_TIMESTAMP", "FROM_UNIXTIME", "SEC_TO_TIME", "TIME_TO_SEC",
+            "ABS", "ACOS", "ASIN", "ATAN", "ATAN2", "CEILING", "COS", "EXP", "FLOOR", "LOG",
+            "LOG10", "MOD", "PI", "POWER", "ROUND", "SIGN", "SIN", "SQRT", "TAN", "TRUNCATE",
+            "ASCII", "CHAR", "CONCAT", "LCASE", "LENGTH", "LTRIM", "REPLACE", "RTRIM", "SOUNDEX",
+            "SUBSTRING", "UCASE", "HOUR", "MINUTE", "SECOND", "MONTH", "YEAR", "abs", "acos", "asin",
+            "atan", "atan2", "ceiling", "cos", "cot", "degrees", "exp", "floor", "log", "log10",
+            "mod", "pi", "power", "radians", "round", "sign", "sin", "sqrt", "tan", "truncate",
+            "ascii", "char", "concat", "lcase", "left", "length", "ltrim", "repeat", "rtrim",
+            "space", "substring", "ucase", "replace", "curdate", "curtime", "dayname", "dayofmonth",
+            "dayofweek", "dayofyear", "hour", "minute", "month", "monthname", "now", "quarter",
+            "second", "week", "year", "timestampadd",};
 
     private void Highlight(SQLScanner.Token token) {
         setCharacterAttributes(token.start, token.end - token.start + 1,
