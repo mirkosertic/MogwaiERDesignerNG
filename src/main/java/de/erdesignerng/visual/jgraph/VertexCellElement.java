e3ae557d8c24064ee088f0278b9a752dd2809ad3
Mirko Sertic <mirko.sertic@web.de>
2011-10-02 11:25:48 +0000

    Merged Intelligent Layout feature back to trunk again, continuing to work on better graph layout

---
+++ src/main/java/de/erdesignerng/visual/jgraph/VertexCellElement.java
@@ -0,0 +1,92 @@
+/**
+ * Mogwai ERDesigner. Copyright (C) 2002 The Mogwai Project.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+package de.erdesignerng.visual.jgraph;
+
+import de.mogwai.layout.graph.Element;
+import org.jgraph.graph.CellView;
+import org.jgraph.graph.GraphCell;
+import org.jgraph.graph.GraphConstants;
+
+import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.geom.Rectangle2D;
+
+public class VertexCellElement extends Element {
+
+    private Dimension size;
+
+    private final CellView view;
+
+    private Point forcePoint;
+
+    private Double radius;
+
+    private Rectangle2D boundaries;
+
+    public VertexCellElement(CellView aView) {
+        view = aView;
+    }
+
+    public GraphCell getCell() {
+        return (GraphCell) view.getCell();
+    }
+
+    @Override
+    public Point getLocation() {
+
+        GraphCell theCell = getCell();
+        Rectangle2D theBounds = GraphConstants.getBounds(theCell.getAttributes());
+        return new Point((int) theBounds.getX(), (int) theBounds.getY());
+    }
+
+    @Override
+    public Dimension getSize() {
+
+        if (size == null) {
+            GraphCell theCell = getCell();
+            Rectangle2D theBounds = GraphConstants.getBounds(theCell.getAttributes());
+            size = new Dimension((int) theBounds.getWidth(), (int) theBounds.getHeight());
+        }
+        return size;
+
+    }
+
+    @Override
+    public double computeRadius() {
+        if (radius == null) {
+            radius = super.computeRadius();
+        }
+        return radius;
+    }
+
+    @Override
+    public Rectangle2D getBoundaries() {
+        if (boundaries == null) {
+            boundaries = super.getBoundaries();
+        }
+        return boundaries;
+    }
+
+    @Override
+    public Point getForcePoint() {
+        if (forcePoint == null) {
+            forcePoint = super.getForcePoint();
+        }
+        return forcePoint;
+    }
+}
