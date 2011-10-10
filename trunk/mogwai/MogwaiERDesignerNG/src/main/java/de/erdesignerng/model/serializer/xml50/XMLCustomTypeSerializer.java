7a3d7f1a74309ad5140fd1483a16ee5604e3764e
dr-death2 <dr-death2@d3b2640a-98bb-4239-95bf-c7bbe60608d9>
2011-10-10 11:14:45 +0000

    added v5.0 serializer:  - CustomTypes  - Domains

---
+++ trunk/mogwai/MogwaiERDesignerNG/src/main/java/de/erdesignerng/model/serializer/xml50/XMLCustomTypeSerializer.java
@@ -0,0 +1,80 @@
+/**
+ * Mogwai ERDesigner. Copyright (C) 2002 The Mogwai Project.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+package de.erdesignerng.model.serializer.xml50;
+
+import de.erdesignerng.model.Attribute;
+import de.erdesignerng.model.CustomType;
+import de.erdesignerng.model.CustomTypeType;
+import de.erdesignerng.model.Model;
+import de.erdesignerng.model.serializer.AbstractXMLModelSerializer;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+/**
+ * @author $Author: dr-death2 $
+ * @version $Date: 2011-10-08 20:00:00 $
+ */
+public class XMLCustomTypeSerializer extends de.erdesignerng.model.serializer.xml40.XMLCustomTypeSerializer {
+
+	public XMLCustomTypeSerializer(AbstractXMLModelSerializer xmlModelSerializer) {
+		super(xmlModelSerializer);
+	}
+
+	@Override
+	public void serialize(CustomType aCustomType, Document aDocument, Element aRootElement) {
+		Element theCustomTypeElement = addElement(aDocument, aRootElement, CUSTOMTYPE);
+
+		// Basisdaten (ID, NAME) des Modelelementes speichern
+		serializeProperties(aDocument, theCustomTypeElement, aCustomType);
+		serializeCommentElement(aDocument, theCustomTypeElement, aCustomType);
+
+		theCustomTypeElement.setAttribute(SCHEMA, aCustomType.getSchema());
+		theCustomTypeElement.setAttribute(TYPE, aCustomType.getType().name());
+		theCustomTypeElement.setAttribute(ALIAS, aCustomType.getAlias());
+
+		// Attribute serialisieren
+		for (Attribute<CustomType> theAttribute : aCustomType.getAttributes()) {
+			getXMLModelSerializer().getXMLAttributeSerializer().serialize(theAttribute, aDocument, theCustomTypeElement);
+		}
+	}
+
+	@Override
+	public void deserialize(Model aModel, Document aDocument) {
+		NodeList theElements = aDocument.getElementsByTagName(CUSTOMTYPE);
+
+		for (int i = 0; i < theElements.getLength(); i++) {
+
+			Element theElement = (Element) theElements.item(i);
+
+			CustomType theCustomType = new CustomType();
+			theCustomType.setOwner(aModel);
+
+			deserializeProperties(theElement, theCustomType);
+			deserializeCommentElement(theElement, theCustomType);
+
+			theCustomType.setSchema(theElement.getAttribute(SCHEMA));
+			theCustomType.setType(CustomTypeType.valueOf(theElement.getAttribute(TYPE)));
+			theCustomType.setSchema(theElement.getAttribute(ALIAS));
+
+			getXMLModelSerializer().getXMLAttributeSerializer().deserialize(aModel, theCustomType, theElement);
+
+			aModel.getCustomTypes().add(theCustomType);
+		}
+	}
+}
---
+++ trunk/mogwai/MogwaiERDesignerNG/src/main/java/de/erdesignerng/model/serializer/xml50/XMLModel50Serializer.java
@@ -0,0 +1,79 @@
+/**
+ * Mogwai ERDesigner. Copyright (C) 2002 The Mogwai Project.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+package de.erdesignerng.model.serializer.xml50;
+
+import de.erdesignerng.model.serializer.AbstractXMLCustomTypeSerializer;
+import de.erdesignerng.model.serializer.AbstractXMLDomainSerializer;
+import de.erdesignerng.model.serializer.AbstractXMLModelSerializer;
+import de.erdesignerng.model.serializer.AbstractXMLSubjectAreaSerializer;
+import de.erdesignerng.model.serializer.xml40.XMLModel40Serializer;
+import de.erdesignerng.util.XMLUtils;
+
+/**
+ * @author $Author: dr-death2 $
+ * @version $Date: 2011-10-08 20:00:00 $
+ */
+public class XMLModel50Serializer extends XMLModel40Serializer {
+
+	private static final String CURRENT_VERSION = "5.0";
+
+	private static final String XML_SCHEMA_DEFINITION = "/erdesignerschema_5.0.xsd";
+
+	public XMLModel50Serializer(XMLUtils utils) {
+		super(utils);
+		setXMLCustomTypeSerializer(new XMLCustomTypeSerializer(this));
+		setXMLDomainSerializer(new XMLDomainSerializer());
+	}
+
+	@Override
+	public String getSchemaResource() {
+		return XML_SCHEMA_DEFINITION;
+	}
+
+	@Override
+	public String getVersion() {
+		return CURRENT_VERSION;
+	}
+
+	@Override
+	public AbstractXMLCustomTypeSerializer getXMLCustomTypeSerializer(AbstractXMLModelSerializer xmlModelSerializer) {
+		if (super.getXMLCustomTypeSerializer(xmlModelSerializer) == null) {
+			setXMLCustomTypeSerializer(new XMLCustomTypeSerializer(xmlModelSerializer));
+		}
+
+		return super.getXMLCustomTypeSerializer(xmlModelSerializer);
+	}
+
+	@Override
+	public AbstractXMLDomainSerializer getXMLDomainSerializer() {
+		if (super.getXMLDomainSerializer() == null) {
+			setXMLDomainSerializer(new XMLDomainSerializer());
+		}
+
+		return super.getXMLDomainSerializer();
+	}
+
+	@Override
+	protected AbstractXMLSubjectAreaSerializer getXMLSubjectAreaSerializer() {
+		if (super.getXMLSubjectAreaSerializer() == null) {
+			setXMLSubjectAreaSerializer(new XMLSubjectAreaSerializer());
+		}
+
+		return super.getXMLSubjectAreaSerializer();
+	}
+}
