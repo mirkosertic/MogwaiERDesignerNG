d33cab8a09763652a224538599c25be2fe8518f1
mirkosertic <mirkosertic@d3b2640a-98bb-4239-95bf-c7bbe60608d9>
2011-11-05 17:49:09 +0000

    Bug Fixing 3429294 [ERDesignerNG] Cannot Delete Comment

---
+++ trunk/mogwai/MogwaiERDesignerNG/src/main/java/de/erdesignerng/visual/common/EditCommentCommand.java
@@ -0,0 +1,46 @@
+/**
+ * Mogwai ERDesigner. Copyright (C) 2002 The Mogwai Project.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+package de.erdesignerng.visual.common;
+
+import de.erdesignerng.model.Comment;
+import de.erdesignerng.visual.editor.comment.CommentEditor;
+
+public class EditCommentCommand extends UICommand {
+
+    private Comment comment;
+    private GenericModelEditor editor;
+
+    public EditCommentCommand(GenericModelEditor aEditor, Comment aComment) {
+        comment = aComment;
+        editor = aEditor;
+    }
+
+    @Override
+    public void execute() {
+        CommentEditor theEditor = new CommentEditor(comment.getOwner(), getDetailComponent());
+        theEditor.initializeFor(comment);
+        if (theEditor.showModal() == CommentEditor.MODAL_RESULT_OK) {
+            try {
+                theEditor.applyValues();
+                editor.repaintGraph();
+            } catch (Exception e) {
+                getWorldConnector().notifyAboutException(e);
+            }
+        }
+    }
+}
