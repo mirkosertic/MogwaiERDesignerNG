d7ee7de6db72de95abdb54417e75b812d1261245
Mirko Sertic <mirko.sertic@web.de>
2011-11-06 15:41:03 +0000

    Bug Fixing

--- src/main/java/de/erdesignerng/model/CascadeType.java
+++ src/main/java/de/erdesignerng/model/CascadeType.java
@@ -22,55 +22,55 @@ package de.erdesignerng.model;
  *
  * @author $Author: mirkosertic $
  * @version $Date: 2008-01-15 19:22:43 $
- * 
- * TODO: [dr-death2] extend this enum from abstract enum implementing the
- * methods and the property "id" if inheritance of enums is supported in java 8
+ *          <p/>
+ *          TODO: [dr-death2] extend this enum from abstract enum implementing the
+ *          methods and the property "id" if inheritance of enums is supported in java 8
  */
 public enum CascadeType {
 
-	// Do *not* rename the constants or change their ids! Their spelling is
-	// used to store the model to *.mxm files and their ids are used to store
-	// the model to the repository. Loading older models would fail then.
-	CASCADE(0),
-	SETNULL(1),
-	NOTHING(2),
-	RESTRICT(3);
+    // Do *not* rename the constants or change their ids! Their spelling is
+    // used to store the model to *.mxm files and their ids are used to store
+    // the model to the repository. Loading older models would fail then.
+    CASCADE(0),
+    SETNULL(1),
+    NOTHING(2),
+    RESTRICT(3);
 
-	private final int id;
+    private final int id;
 
-	CascadeType(final int id) {
-		this.id = id;
-	}
+    CascadeType(final int id) {
+        this.id = id;
+    }
 
-	public final int getId() {
-		return id;
-	}
+    public final int getId() {
+        return id;
+    }
 
-	/**
-	 * Returns the enum constant of this type with the specified id.
-	 *
-	 * @param id the id to return the enum constant for
-	 * @return the enum constant with the specified id
-	 * @throws IllegalArgumentException if this enum type has no constant with the specified id
-	 */
-	public static CascadeType fromId(int id) {
+    /**
+     * Returns the enum constant of this type with the specified id.
+     *
+     * @param id the id to return the enum constant for
+     * @return the enum constant with the specified id
+     * @throws IllegalArgumentException if this enum type has no constant with the specified id
+     */
+    public static CascadeType fromId(int id) {
 
-		for(CascadeType aCascadeType : CascadeType.values()) { 
-			if (aCascadeType.getId() == id) {
-				return aCascadeType;
-			}
-		}
+        for (CascadeType aCascadeType : CascadeType.values()) {
+            if (aCascadeType.getId() == id) {
+                return aCascadeType;
+            }
+        }
 
-		throw new IllegalArgumentException("Unknown type \"" + id + "\"!");
-	}
+        throw new IllegalArgumentException("Unknown type \"" + id + "\"!");
+    }
 
-	public static CascadeType fromString(String aConstantName) {
-		return CascadeType.valueOf(aConstantName.toUpperCase());
-	}
+    public static CascadeType fromString(String aConstantName) {
+        return CascadeType.valueOf(aConstantName.toUpperCase().trim());
+    }
 
-	@Override
-	public final String toString() {
-		return super.toString().toUpperCase();
-	}
+    @Override
+    public final String toString() {
+        return super.toString().toUpperCase();
+    }
 
 }
